248456fe493bc47942a14b912bdc7cede555b458
Mira Partha <mirapartha@gmail.com>
2020-11-02 22:01:51 -0800

    Update generators.py
    Fixed function declarations

--- generators.py
+++ generators.py
@@ -1,8 +1,9 @@
+from typing import List
 from random import random
 import numpy as np
 
 class generators():
-    def random_gen(masked_template: str, num_seq: int, vocab=['L', 'A', 'G', 'V', 'S', 'E', 'R', 'T', 'I', 'D', 'P', 'K', 'Q', 'N', 'F', 'Y', 'M', 'H', 'W', 'C', 'X', 'B', 'U', 'Z', 'O']: List[str]) -> List[str]:
+    def random_gen(masked_template: str, num_seq: int, vocab : List[str] = ['L', 'A', 'G', 'V', 'S', 'E', 'R', 'T', 'I', 'D', 'P', 'K', 'Q', 'N', 'F', 'Y', 'M', 'H', 'W', 'C', 'X', 'B', 'U', 'Z', 'O']) -> List[str]:
         random_seqs = []
         for n in range(num_seq):
             mutant=''
@@ -14,5 +15,5 @@ class generators():
             random_seqs.append(mutant)
         return random_seqs
 
-    def substitution_gen(unmasked_template: str, num_seq: int, submat: np.array, vocab=['L', 'A', 'G', 'V', 'S', 'E', 'R', 'T', 'I', 'D', 'P', 'K', 'Q', 'N', 'F', 'Y', 'M', 'H', 'W', 'C', 'X', 'B', 'U', 'Z', 'O']: List[str]) -> List[str]:
+    def substitution_gen(unmasked_template: str, num_seq: int, submat: np.array, vocab : List[str] = ['L', 'A', 'G', 'V', 'S', 'E', 'R', 'T', 'I', 'D', 'P', 'K', 'Q', 'N', 'F', 'Y', 'M', 'H', 'W', 'C', 'X', 'B', 'U', 'Z', 'O']) -> List[str]:
         pass
