0647113d884a0d68eeaaac745fce6805db33fcbc
Mira Partha <mirapartha@gmail.com>
2020-11-13 00:25:12 -0800

    Changed to write output to .fasta file

--- generators.py
+++ generators.py
@@ -3,7 +3,7 @@ from random import random
 import numpy as np
 
 class generators():
-    def random_gen(masked_template: str, num_seq: int, vocab : List[str] = ['L', 'A', 'G', 'V', 'S', 'E', 'R', 'T', 'I', 'D', 'P', 'K', 'Q', 'N', 'F', 'Y', 'M', 'H', 'W', 'C', 'X', 'B', 'U', 'Z', 'O']) -> List[str]:
+    def random_gen(masked_template: str, num_seq: int, output_path: str, vocab : List[str] = ['L', 'A', 'G', 'V', 'S', 'E', 'R', 'T', 'I', 'D', 'P', 'K', 'Q', 'N', 'F', 'Y', 'M', 'H', 'W', 'C', 'X', 'B', 'U', 'Z', 'O']) -> List[str]:
         random_seqs = set()
         #for n in range(num_seq):
         while len(random_seqs) < num_seq:
@@ -14,7 +14,15 @@ class generators():
                 else:
                     mutant += residue
             random_seqs.add(mutant)
-        return list(random_seqs)
+        random_seqs = list(random_seqs)
+
+        #Write sequences to .fasta file instead
+        ofile = open(output_path, "w")               # e.g. 'directory/subdirectory/randomseqs.fasta'
+        for i in range(len(random_seqs)):
+            ofile.write(">random_seq" + str(i+1) + "\n" + random_seqs[i] + "\n")
+        ofile.close()
+
+        return random_seqs
 
     def substitution_gen(unmasked_template: str, num_seq: int, submat: np.array, vocab : List[str] = ['L', 'A', 'G', 'V', 'S', 'E', 'R', 'T', 'I', 'D', 'P', 'K', 'Q', 'N', 'F', 'Y', 'M', 'H', 'W', 'C', 'X', 'B', 'U', 'Z', 'O']) -> List[str]:
         pass
