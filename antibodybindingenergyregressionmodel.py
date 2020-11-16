4511501f29d65ad35ab167b29a7192a019b4d257
Felipe Calero Forero <felipecalerof@gmail.com>
2020-11-16 00:31:06 -0700

    Update antibodybindingenergyregressionmodel.py

--- antibodybindingenergyregressionmodel.py
+++ antibodybindingenergyregressionmodel.py
@@ -8,80 +8,75 @@ Original file is located at
 """
 
 import numpy as np
-# import torch
-# import torch.nn as nn
-# import keras
-# from keras.models import Sequential
-# from keras.layers import Dense
+import torch
+import torch.nn as nn
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
 import workflow
 
 
-# subset_200_seq85k_embeddings = load_seqs_and_embeddings('subset_200_seq85k', False, df)
 
-# subset = subset_200_seq85k_embeddings
+def dataLoader(keys, batch_size):
+    L = len(keys)
+    df = workflow.import_energy_metadata()
+    #this line is just to make the generator infinite, keras needs that apparently    
+    while True:
 
-# embeddings = []
-# scores = []
+        batch_start = 0
+        batch_end = batch_size
 
-# for key in subset.keys():
-#   embedding = torch.unsqueeze(subset[key]['token_embeddings'], 0)
-#   embeddings.append(embedding)
-#   score = np.zeros(3)
-#   score[0] = subset[key]['FoldX_Average_Whole_Model_DDG']
-#   score[1] = subset[key]['FoldX_Average_Interface_Only_DDG']
-#   score[2] = subset[key]['Statium']
-#   scores.append(score)
-# X = torch.cat(embeddings, dim=0)
-# X = torch.flatten(X, start_dim=1, end_dim=-1)
-# X = X.numpy()
+        while batch_start < L:
+            limit = min(batch_end, L)
+            X = workflow.load_embeddings(name, (keys[batch_start:limit]))
+            Y = workflow.load_energy_metadata((keys[batch_start:limit]), df)
+            Y = Y[:, 0:1]
 
-# Y = np.stack(scores)
+            yield (X,Y) #a tuple with two numpy arrays with batch_size samples     
 
-# X = keras.utils.normalize(X, axis=-1, order=2)
+            batch_start += batch_size   
+            batch_end += batch_size
+
+
+def RegressionModel(input_shape, dropout=.2):
+    X_input = keras.Input(input_shape)
+    X = keras.layers.Dropout(dropout)(X)
+
+
+    X = Dense(800, activation='relu', kernel_initializer="he_uniform")(X)
+    X = keras.layers.BatchNormalization(axis=-1, momentum=0.99)(X)
+    X = keras.layers.Dropout(dropout)(X)
+
+
+    X = Dense(700, activation='relu', kernel_initializer="he_uniform")(X)
+    X = keras.layers.BatchNormalization(axis=-1, momentum=0.99)(X)
+    X = keras.layers.Dropout(dropout)(X)
+
+
+    X = Dense(400, activation='relu', kernel_initializer="he_uniform")(X)
+    X = keras.layers.BatchNormalization(axis=-1, momentum=0.99)(X)
+    X = keras.layers.Dropout(dropout)(X)
+
+
+    X = Dense(3, kernel_initializer="he_uniform")(X)
+
+    model = keras.Model(inputs = X_input, outputs = X, name='RegressionModel')
+
+    return model
+
+dropout = .2
+input_shape = 585782
+model = RegressionModel(input_shape, dropout)
+
+model.compile(optimizer='adam', loss=keras.losses.MeanSquaredError())
+
 name = 'subset_200_seq85k'
+batch_size = 50
+step_size = 4
 
-df = workflow.import_energy_metadata()
-X_train = workflow.get_embedding_list(name)
-Y = workflow.load_energy_metadata(X_train, df)
-Y_train = Y[:, 0:1]
-
-workflow.load_embeddings(name, keys)
+keys = workflow.get_embedding_list(name)
 
 
-# input_shape = 1
 
-# dropout = .2
-
-# def RegressionModel(input_shape, dropout=.2):
-#     X_input = keras.Input(input_shape)
-
-#     X = keras.layers.Lambda(lambda batch: workflow.load_embeddings(name, batch), dynamic=True)(X_input)
-#     X = keras.layers.Dropout(dropout)(X)
-
-
-#     X = Dense(800, activation='relu', kernel_initializer="he_uniform")(X)
-#     X = keras.layers.BatchNormalization(axis=-1, momentum=0.99)(X)
-#     X = keras.layers.Dropout(dropout)(X)
-
-
-#     X = Dense(700, activation='relu', kernel_initializer="he_uniform")(X)
-#     X = keras.layers.BatchNormalization(axis=-1, momentum=0.99)(X)
-#     X = keras.layers.Dropout(dropout)(X)
-
-
-#     X = Dense(400, activation='relu', kernel_initializer="he_uniform")(X)
-#     X = keras.layers.BatchNormalization(axis=-1, momentum=0.99)(X)
-#     X = keras.layers.Dropout(dropout)(X)
-
-
-#     X = Dense(3, kernel_initializer="he_uniform")(X)
-
-#     model = keras.Model(inputs = X_input, outputs = X, name='RegressionModel')
-
-#     return model
-
-# model = RegressionModel(input_shape)
-
-# model.compile(optimizer='adam', loss=keras.losses.MeanSquaredError())
-
-# model.fit(x=X_train, y=Y_train, batch_size=2, epochs=1)
+# Steps per epoch is size of keys divided batch_size
+model.fit(x=dataLoader(keys, batch_size), steps_per_epoch=step_size, epochs = 1)
